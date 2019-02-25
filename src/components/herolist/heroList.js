ed61fdd7ac4853eae540954ed3e9a10a39bf47a8
Mitchell Carter <carter.svo@gmail.com>
2019-02-24 21:16:42 -0500

    renamed image file

--- src/components/herolist/heroList.js
+++ src/components/herolist/heroList.js
@@ -46,7 +46,7 @@ class HeroList extends Component {
               <p>Naruto Uzumaki (うずまきナルト, Uzumaki Naruto) is a shinobi of Konohagakure's Uzumaki clan. He became the jinchūriki of the Nine-Tails on the day of his birth — a fate that caused him to be shunned by most of Konoha throughout his childhood. After joining Team Kakashi, Naruto worked hard to gain the village's acknowledgement all the while chasing his dream to become Hokage. In the following years, through many hardships and ordeals, he became a capable ninja regarded as a hero both by the villagers, and soon after, the rest of the world, becoming known as the Hero of the Hidden Leaf (木ノ葉隠れの英雄, Konohagakure no Eiyū, Literally meaning: Hero of the Hidden Tree Leaves). He soon proved to be one of the main factors in winning the Fourth Shinobi World War, leading him to achieve his dream and become the village's Seventh Hokage (七代目火影, Nanadaime Hokage, Literally meaning: Seventh Fire Shadow).</p>
               
               <div className='heroes-list__back'>
-                <a href='https://www.youtube.com/watch?v=lJzCsj3aP3k'><img className='heroes-list__image' src="../assets/images/naruto.PNG"/></a>
+                <a href='https://www.youtube.com/watch?v=lJzCsj3aP3k'><img className='heroes-list__image' src="../assets/images/naruto.png"/></a>
               </div>
 
             </div>
